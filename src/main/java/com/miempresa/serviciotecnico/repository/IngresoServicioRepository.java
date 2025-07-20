32c12cf8352e0af3379de34fa3086d25ad9d0acf
JesusAlbornoz29 <alfredo.albornoz@estudiantes.utec.edu.uy>
2025-07-19 22:36:19 -0300

    "Creando el repository IngresoServicio"

---
+++ src/main/java/com/miempresa/serviciotecnico/repository/IngresoServicioRepository.java
@@ -0,0 +1,7 @@
+package com.miempresa.serviciotecnico.repository;
+
+import com.miempresa.serviciotecnico.model.IngresoServicio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngresoServicioRepository extends JpaRepository<IngresoServicio, Long> {
+}
