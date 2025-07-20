92678fcb7e7bdde86d7a67de9a9d302f95a7e2df
JesusAlbornoz29 <alfredo.albornoz@estudiantes.utec.edu.uy>
2025-07-19 23:31:29 -0300

    "Creado ComentarioIngreso Entity"

---
+++ src/main/java/com/miempresa/serviciotecnico/model/ComentarioIngreso.java
@@ -0,0 +1,28 @@
+package com.miempresa.serviciotecnico.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class ComentarioIngreso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String contenido;
+    private String autor;
+    private LocalDateTime fechaHora;
+
+    @ManyToOne
+    @JoinColumn(name = "ingreso_id")
+    private IngresoServicio ingresoServicio;
+}
