64a4be5fa9cd3a23f2c06c387619e39f02c241d7
JesusAlbornoz29 <alfredo.albornoz@estudiantes.utec.edu.uy>
2025-07-24 11:15:21 -0300

    "Agregamos que el atributo cedula de la entidad cliente no pueda ser null"

--- src/main/java/com/miempresa/serviciotecnico/model/Cliente.java
+++ src/main/java/com/miempresa/serviciotecnico/model/Cliente.java
@@ -15,7 +15,7 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String cedula;
 
     private String nombre;
