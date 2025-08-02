2363e02d0b7baad7358e83ee25df3c3bd3769a31
JesusAlbornoz29 <alfredo.albornoz@estudiantes.utec.edu.uy>
2025-08-02 12:56:39 -0300

    "Comenzando con entidad Proveedor"

---
+++ src/main/java/com/miempresa/serviciotecnico/model/Proveedor.java
@@ -0,0 +1,27 @@
+package com.miempresa.serviciotecnico.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Proveedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    private String direccion;
+    private String telefono;
+    private String email;
+}
